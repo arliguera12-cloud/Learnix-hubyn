1559967992376288f26af02231a37cd6975f2fdd
arliguera12-cloud <arliguera12@gmail.com>
2026-05-02 14:26:50 -0600

    Initialize models module with docstring and exports
    Added module docstring and defined exports.

--- core/models/_init_.py
+++ core/models/_init_.py
@@ -1,1 +1,13 @@
+# core/modelos/__init__.py
+"""
+Módulo de modelos y esquemas de datos.
+"""
 
+from .constantes import *
+from .schemas import *
+
+__all__ = [
+    "TIPOS_DTE",
+    "ANEXOS_DISPONIBLES",
+    "DTE_SCHEMA",
+]
